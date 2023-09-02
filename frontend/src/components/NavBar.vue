317dea753a195ee882f62b89febda9ab4422f282
thenotrealagata <49726461+thenotrealagata@users.noreply.github.com>
2023-09-02 22:50:04 +0200

    start user favourite songs API request

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -3,10 +3,14 @@
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">music trivia!!</a>
         </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li><router-link to="/">home</router-link></li>
+              <li><router-link to="/play">play</router-link></li>
+              <li><router-link to="/about">about</router-link></li>
+            </ul>
           </div>
+        <div className="flex-none gap-2">
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -5,9 +5,9 @@
     <h2>...not ready yet?</h2>
     <p>Here's some data about your most listened to tracks in the past year!</p>
     <button v-if="isLoggedIn" @click="authoriseSpotify">But first, connect to Spotify!</button>
-    <div v-else>
+    <div>
         Your most listened songs this year are...
-        
+        <!--<div v-for="favouriteSong in favouriteSongs" v-bind:key="favouriteSong.name">{{ favouriteSong.name }}</div>-->
     </div>
   </div>
 </template>
@@ -16,25 +16,30 @@
 export default {
     name: "HomePage",
     data() {
-        return {
-            loggedin: false,
-        }
+      return {
+        favouriteSongs: [],
+      }
     },
     methods: {
         authoriseSpotify() {
             handleLogin();
-        }
+        },
     },
     computed: {
         isLoggedIn() {
-            return localStorage.getItem('access_token') == null;
-        }
+          return !(localStorage.getItem('access_token') == null) && true; // TODO: replace true with when new access token needs to be generated 
+        },
+    },
+    async created() {
+        
     }
 }
 
+// App details
 const clientId = '956e9f7af3b34a0293c7aba306c36eb3';
 const redirectUri = 'http://localhost:8080/';
 
+// Authorise Spotify
 async function handleLogin() {
   function generateRandomString(length) {
     let text = '';
@@ -65,7 +70,7 @@ async function handleLogin() {
 
   generateCodeChallenge(codeVerifier).then(codeChallenge => {
     let state = generateRandomString(16);
-    let scope = 'user-read-private user-read-email';
+    let scope = 'user-read-private user-read-email user-top-read';
 
     localStorage.setItem('code_verifier', codeVerifier);
 
@@ -83,6 +88,7 @@ async function handleLogin() {
   });
 }
 
+// Generating user token
 const urlParams = new URLSearchParams(window.location.search);
 let code = urlParams.get('code');
 let codeVerifier = localStorage.getItem('code_verifier');
@@ -95,7 +101,7 @@ let body = new URLSearchParams({
   code_verifier: codeVerifier
 });
 
-const response = fetch('https://accounts.spotify.com/api/token', {
+fetch('https://accounts.spotify.com/api/token', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -112,10 +118,8 @@ const response = fetch('https://accounts.spotify.com/api/token', {
     console.error('Error:', error);
 });
 
-console.log(response); // comment this out pls
-
+// Example function: access user profile (name, images, id, country, ...)
 async function getProfile(accessToken) {
-    console.log('access token: ' + localStorage.getItem('access_token'));
     accessToken = localStorage.getItem('access_token');
 
     const response = await fetch('https://api.spotify.com/v1/me', {
@@ -130,6 +134,24 @@ async function getProfile(accessToken) {
 }
 
 getProfile();
+
+async function getTopTracks(accessToken){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  accessToken = localStorage.getItem('access_token');
+
+  const response = await fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+}
+
+getTopTracks();
 </script>
 
 <style>
